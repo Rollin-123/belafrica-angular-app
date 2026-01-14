946957131894899b09c9698c670ecd469e163fa6
Rollin Loic <rollinloictianga@gmail.com>
2026-01-14 03:57:52 +0300

    gestion de lapp en PWA

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,7 +3,7 @@
     * Copyright © 2025 Rollin Loic Tianga. Tous droits réservés.
     * Code source confidentiel - Usage interdit sans autorisation
     */
-import { RenderMode, ServerRoute } from '@angular/common/http';
+import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
